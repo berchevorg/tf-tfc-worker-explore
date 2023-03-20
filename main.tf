a944cb13180229e30ec9ad7285f201686569550f
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2023-03-20 07:56:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,57 +1,57 @@
-# resource "null_resource" "files" {
-#   provisioner "local-exec" {
-#     command = "find | sed 's|[^/]*/|- |g'"
-#   }
-#   triggers = {
-#     run_every_time = uuid()
-#   }
-# }
+resource "null_resource" "files" {
+  provisioner "local-exec" {
+    command = "find | sed 's|[^/]*/|- |g'"
+  }
+  triggers = {
+    run_every_time = uuid()
+  }
+}
 
-# resource "null_resource" "env" {
-#   provisioner "local-exec" {
-#     command = "env"
-#   }
-#   triggers = {
-#     run_every_time = uuid()
-#   }
-# }
+resource "null_resource" "env" {
+  provisioner "local-exec" {
+    command = "env"
+  }
+  triggers = {
+    run_every_time = uuid()
+  }
+}
 
-# resource "null_resource" "cat-backend-override" {
-#   provisioner "local-exec" {
-#     command = "cat zzz_backend_override.tf.json"
-#   }
-#   triggers = {
-#     run_every_time = uuid()
-#   }
-# }
+resource "null_resource" "cat-backend-override" {
+  provisioner "local-exec" {
+    command = "cat zzz_backend_override.tf.json"
+  }
+  triggers = {
+    run_every_time = uuid()
+  }
+}
 
-# resource "null_resource" "mem-check" {
-#   provisioner "local-exec" {
-#     #command = "cat /proc/meminfo | grep 'MemTotal'"
-#     command = "cat /proc/meminfo"
-#   }
-#   triggers = {
-#     run_every_time = uuid()
-#   }
-# }
+resource "null_resource" "mem-check" {
+  provisioner "local-exec" {
+    #command = "cat /proc/meminfo | grep 'MemTotal'"
+    command = "cat /proc/meminfo"
+  }
+  triggers = {
+    run_every_time = uuid()
+  }
+}
 
-# resource "null_resource" "run_command" {
-#   provisioner "local-exec" {
-#     command = "/usr/bin/python3 --version"
-#   }
-#   triggers = {
-#     run_every_time = uuid()
-#   }
-# }
+resource "null_resource" "run_command" {
+  provisioner "local-exec" {
+    command = "/usr/bin/python3 --version"
+  }
+  triggers = {
+    run_every_time = uuid()
+  }
+}
 
-# resource "null_resource" "pwd" {
-#   provisioner "local-exec" {
-#     command = "pwd"
-#   }
-#   triggers = {
-#     run_every_time = uuid()
-#   }
-# }
+resource "null_resource" "pwd" {
+  provisioner "local-exec" {
+    command = "pwd"
+  }
+  triggers = {
+    run_every_time = uuid()
+  }
+}
 
 resource "null_resource" "aws" {
   provisioner "local-exec" {
